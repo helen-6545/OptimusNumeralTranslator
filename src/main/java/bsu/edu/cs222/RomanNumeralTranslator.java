1bf2c358099ad1cd3e1f3b667e9fde1873b9ee66
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2023-03-23 12:50:29 -0400

    Adds tests and beginning classes

---
+++ src/main/java/bsu/edu/cs222/RomanNumeralTranslator.java
@@ -0,0 +1,58 @@
+package bsu.edu.cs222;
+
+public class RomanNumeralTranslator {
+
+    public static String thousandsTranslator(int input){
+        return switch (input) {
+            case 1 -> "M";
+            case 2 -> "MM";
+            case 3 -> "MMM";
+            default -> "";
+        };
+    }
+    public static String hundredsTranslator(int input){
+        return switch (input) {
+            case 1 -> "C";
+            case 2 -> "CC";
+            case 3 -> "CCC";
+            case 4 -> "CD";
+            case 5 -> "D";
+            case 6 -> "DC";
+            case 7 -> "DCC";
+            case 8 -> "DCCC";
+            case 9 -> "CM";
+            default -> "";
+        };
+    }
+    public static String tensTranslator(int input){
+        return switch (input) {
+            case 1 -> "X";
+            case 2 -> "XX";
+            case 3 -> "XXX";
+            case 4 -> "XL";
+            case 5 -> "L";
+            case 6 -> "LX";
+            case 7 -> "LXX";
+            case 8 -> "LXXX";
+            case 9 -> "XC";
+            default -> "";
+        };
+    }
+    public static String onesTranslator(int input){
+        return switch (input) {
+            case 1 -> "I";
+            case 2 -> "II";
+            case 3 -> "III";
+            case 4 -> "IV";
+            case 5 -> "V";
+            case 6 -> "VI";
+            case 7 -> "VII";
+            case 8 -> "VIII";
+            case 9 -> "IX";
+            default -> "";
+        };
+    }
+    public static boolean OutOfRangeCheck(int input){
+        return (input >= 1) && (input <= 3000);
+    }
+}
---
+++ src/test/java/bsu/edu/cs222/RomanNumeralTest.java
@@ -0,0 +1,43 @@
+package bsu.edu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class RomanNumeralTest {
+
+    @Test
+    public void OutOfRangeTestLow() {
+        int input = 0;
+        boolean test=RomanNumeralTranslator.OutOfRangeCheck(input);
+        Assertions.assertFalse(test);
+    }
+
+    @Test
+    public void OutOfRangeTestHigh() {
+        int input = 3001;
+        boolean test=RomanNumeralTranslator.OutOfRangeCheck(input);
+        Assertions.assertFalse(test);
+    }
+
+    @Test
+    public void TestFive() {
+        int input = 5;
+        String test=RomanNumeralTranslator.onesTranslator(input);
+        Assertions.assertEquals("V", test);
+    }
+
+    @Test
+    public void TestEight() {
+        int input = 8;
+        String test=RomanNumeralTranslator.onesTranslator(input);
+        Assertions.assertEquals("VIII", test);
+    }
+
+    @Test
+    public void TestFifty() {
+        int input = 5;
+        String test=RomanNumeralTranslator.tensTranslator(input);
+        Assertions.assertEquals("L", test);
+    }
+}
